46616a7795d62e361b9bbe0efa359397792d6a1b
chilemati <amadichile007@gmail.com>
2024-09-22 22:50:02 +0100

    v4

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
 import { cartData } from '../atoms/cart'
 
@@ -12,10 +12,19 @@ const Cart = () => {
 
           setCart(removed);
     }
-     function getTotal() {
-       
-        return 0;
-     }
+    useEffect(()=> {
+        if(cart.length > 0) {
+           let sum = cart.map((a)=> {
+           let y = Number(String(a.price).split(' ')[1].split('.000')[0])
+               return y;
+            });
+
+           setTotal(sum.reduce((a,b)=> a+=b))
+
+        }else{
+            console.log('cart empty')
+        }
+     },[cart])
   return (
     <div>
         <h3>Cart Items</h3>
@@ -26,7 +35,7 @@ const Cart = () => {
                 </div>
             ))
         }
-        <h3>Total Price: {getTotal()} </h3>
+        <h3>Total Price: Rp {String(total).includes('.')?String(total).padEnd(5,'0').concat('.000'):String(total).concat('.').padEnd(5,'0').concat('.000')} </h3>
     </div>
   )
 }
