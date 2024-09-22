4b3eea51421d6a1572f2ec06cec5645a1ba25ccd
chilemati <amadichile007@gmail.com>
2024-09-22 21:46:22 +0100

    v3

--- src/components/single/SingleProduct.jsx
+++ src/components/single/SingleProduct.jsx
@@ -2,33 +2,47 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { products } from "../data/product.json";
 import axios from 'axios';
+import { Image } from 'cloudinary-react';
 
 const SingleProduct = () => {
 
     let {id} = useParams();
     let [data,setData] = useState(null);
     useEffect(()=> {
-    //    products.map(each=> {
-    //     if(each.id == id) {
-    //         setData(each);
-    //     }
-    //    })
-    axios.get('http://localhost:9000/products/'+id)
-    .then(resp=> {
-        console.log(resp);
-        setData(resp.data);
-    })
-    .catch(err=> {
-        console.log(err)
-    })
+       products.map(each=> {
+        if(each.id == id) {
+            setData(each);
+        }
+       })
+    // axios.get('http://localhost:9000/products/'+id)
+    // .then(resp=> {
+    //     console.log(resp);
+    //     setData(resp.data);
+    // })
+    // .catch(err=> {
+    //     console.log(err)
+    // })
+
+    /* 
+      how to free host on varcel
+      > make sure your react is error free
+      > npm run build
+      > login or signup to varcel
+      > push your project to github
+      > connect your github to your varcel account
+      > import from git
+      > set some enviromental variable
+      > deploy
+     */
     },[])
 
   return (
-    <div>
+    <div className="flex items-center justify-center flex-col flex-wrap gap-4 ">
         <h3>SingleProduct | {id}</h3>
         {
             data && <div>
               <h4> {data.name} </h4>
+              <Image className=' ' cloudName='dlifiojbx' publicId={data.img} loading='lazy' />
             </div>
         }
          </div>
